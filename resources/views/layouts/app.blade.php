8bb1253c21aff10a0b28e77d3f696491f8b3c74d
Kevin Skompinski <kevons5252@gmail.com>
2017-01-31 23:14:32 -0500

    Navbar and Footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,20 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0f7ecc">
+    <meta name="theme-color" content="#ffffff">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Info -->
-    <title>{{ config('app.name', 'Discreet Drop') }}</title>
-    <meta name="description" content="">
+    <title>Discreet Drop - @yield('subtitle', 'Guarantee Discreet Shipping on Any Package')</title>
+    <meta name="description" content="@yield('description', 'Hide logos, return labels, invoices, holes, and all other identifying information on any package from any seller so your item arrives as discreet as possible.')">
 
     <!-- Scripts -->
     <script>
@@ -28,7 +35,7 @@
     @yield('header')
 </head>
 <body>
-    <nav class="navbar navbar-default navbar-fixed-top">
+    <nav class="navbar navbar-default navbar-fixed-top" id="mainNav">
         <div class="container">
             <div class="navbar-header">
 
@@ -41,9 +48,7 @@
                 </button>
 
                 <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Discreet Drop') }}
-                </a>
+                <a class="navbar-brand logo" href="{{ url('/') }}"><img class="img-responsive" alt="Discreet Drop Logo" src="/images/DiscreetDropLogo.svg"></a>
             </div>
 
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
@@ -83,19 +88,18 @@
         @yield('content')
     </div>
 
-    <footer class = "container">
+    <footer class="footer">
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha256-/SIrNqv8h6QGKDuNoLGA4iret+kyesCkHGzVUUV0shc=" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        <div class="footer-container" >
-            <div class="footer-content">
-                <div class = "row">
-                    <hr>
-                    <div class="col-sm-4"><p>&copy; Discreet Drop <?php echo date("Y"); ?></p></div>
-                    <div class="col-sm-4"><p class="text-center">Made with &#x2764; in Buffalo, NY.</p></div>
-                    <div class="col-sm-4"><a href="/about" class="pull-right">About This Application</a></div>
-                </div>
+        <div class="container" >
+
+            <div class = "row">
+                <div class="col-sm-4"><p>&copy; Discreet Drop <?php echo date("Y"); ?></p></div>
+                <div class="col-sm-4"><p class="text-center">Made with &#x2764; in Buffalo, NY.</p></div>
+                <div class="col-sm-4"><a href="/about" class="pull-right">About This Application</a></div>
             </div>
+            
         </div>
         @yield('footer')
         <script src="/js/app.js"></script>
