ef4f73dc7d1b658a715377d1ebbe90515a57dd1a
Kevin Skompinski <kevons5252@gmail.com>
2017-03-10 00:26:03 -0500

    Revert "Outgoing Package Delivery Check at 3:00am Every Day"
    This reverts commit fd7d5c191503fbc90d62dba90ad2d177aab76406.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,10 +5,6 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
-use App\Order;
-use App\Shipment;
-use App\Outgoing_Package;
-
 class Kernel extends ConsoleKernel
 {
     /**
@@ -28,22 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call(function () {
-            \EasyPost\EasyPost::setApiKey(config('services.easypost.key'));
-            $shipments = Shipment::where('outgoing_package_status', 'Shipped')->get();
-            $outgoing_package
-            foreach($shipments as $shipment){
-                $outgoing_package = Shipment::where('shipment_id', $shipment->id);
-                $order = Order::where('shipment_id', $shipment->id);
-                $tracker = \EasyPost\Tracker::retrieve($outgoing_package->tracker_id);
-                if($tracker->status == 'delivered'){
-                    $shipment->outgoing_package_status = "Delivered";
-                    $outgoing_package->status = $tracker->status;
-                    $order->shipment_status = "Delivered";
-                    $order->order_status = "Delivered";
-                }
-            }
-        })->dailyAt('3:00');
+        // $schedule->command('inspire')
+        //          ->hourly();
     }
 
     /**
