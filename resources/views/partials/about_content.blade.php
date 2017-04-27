65855d22bab1826deed99109c85ff2b74a318517
Kevin Skompinski <kevons5252@gmail.com>
2017-04-27 01:12:28 -0400

    Press and Style Guide Finished

---
+++ resources/views/partials/about_content.blade.php
@@ -0,0 +1,51 @@
+<div class="section">
+    <div class="row">
+        <div class="col-sm-10 col-sm-offset-1">
+            <h2>What is Discreet Drop?</h2>
+            <p>Discreet Drop is a Buffalo-based startup founded in 2017 by Kevin Skompinski, serving to create a way to make sure that every item people order can arrive to them as discreet as possible. Privacy has become a pseudo-luxury in today's digital age, with any information being available just a simple search way. We've identifed this gap in privacy protection with all the information and identifiable logos a shipped box provides to anyone who may see it. Even merchants who advertise discreet plain-box shipping, still provide a return label with a name and address where one quick Google search would reveal what's in any package. We've realized that if you're not handling your package at all times, your privacy is vulnerable on any shipment.</p>
+        </div>
+    </div>
+</div>
+    
+<div class="section">
+    <div class="row">
+        <div class="col-sm-6 col-sm-offset-3">
+            <img src="images/DiscreetDropLogo.svg" class="img-responsive center-block svg" alt="Responsive image">
+        </div>
+    </div>
+</div>
+    
+<div class="section">
+    <div class="row">
+        <div class="col-sm-10 col-sm-offset-1">
+            <h2>Yes, but what do you do?</h2>
+            <p>With this in mind, we created Discreet Drop as a solution to cover up those privacy issues, literally. We serve as a shipment processor, where people can ship their packages to us via a unique address, we double-box the package to cover up any external logos, return labels, packing slips, holes; anything that can act as identifying information and can be used to signal what's in the box. Then we ship the plain box out to the customer so it will arrive completely discreet, and nobody but the customer would be able to figure out what's in the package. Easy. </p>
+        </div>
+    </div>
+</div>
+<div class="section">
+    <div class="row vertical-align">
+        <div class="col-sm-10 col-sm-offset-1">
+            <h2>Our Team</h2>
+
+            <div class="row">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-xs-6 col-sm-3 col-xs-offset-3 col-sm-offset-0 col-sm-push-9">
+                                <img src="http://placeholder.pics/svg/400x400" class="img-responsive center-block svg img-circle" alt="Responsive image">
+                                <br>
+                            </div>
+                            <div class="col-sm-9 col-xs-12 col-sm-pull-3">
+                                Kevin Skompinski is an entrepenuer and software developer who graduated from the University at Buffalo in 2013, and is the founder of Discreet Drop. I've had the idea for discreet Drop for a few years now, and after learning the necessary tools to build modern web-applications, decided to finally craft my own startup from scratch with as much care and attention to detail as possible. I now serve as CEO, sole developer, and package processor.<br><br>
+                                <a href="https://www.linkedin.com/in/kevin-skompinski-68b4231a/" target="_blank">Connect with Kevin Skompinski on LinkedIn.</a><br>
+                                <a href="https://github.com/Kevon" target="_blank">See Kevin's other projects on GitHub.</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
--- resources/views/press_and_style_guide.blade.php
+++ resources/views/press_and_style_guide.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('subtitle', 'Press and Style Guide')
-@section('description', 'Discreet Drop press information, logos, colors, videos, and style guide.')
+@section('description', 'Get logos, icons, colors, videos, and press information for Discreet Drop.')
 
 
 @section('header')
@@ -19,7 +19,82 @@
 
 <div class="container content">
     
+    <div class="section">
+        <h2 class="center">Discreet Drop Logo</h2>
+        <div class="row">
+            <div class="col-sm-6">
+                <h3 class="center">SVG</h3>
+                <img src="images/DiscreetDropLogo.svg" class="img-responsive center-block svg" alt="Discreet Drop Logo SVG">
+            </div>
+            <div class="col-sm-6">
+                <h3 class="center">PNG</h3>
+                <img src="images/DiscreetDropLogo.png" class="img-responsive center-block svg" alt="Discreet Drop Logo PNG">
+            </div>
+        </div>
+    </div>
 
+    <div class="section">
+        <h2 class="center">Discreet Drop Icon</h2>
+        <div class="row">
+            <div class="col-xs-3 col-xs-offset-2">
+                <h3 class="center">SVG</h3>
+                <img src="images/DiscreetDropIcon.svg" class="img-responsive center-block svg" alt="Discreet Drop Icon SVG">
+            </div>
+            <div class="col-xs-3 col-xs-offset-2">
+                <h3 class="center">PNG</h3>
+                <img src="images/DiscreetDropIcon.png" class="img-responsive center-block svg" alt="Discreet Drop Icon PNG">
+            </div>
+        </div>
+    </div>
+    
+    <div class="section">
+        <h2 class="center">Colors</h2>
+        <div class="row always-equal">
+            <div class="col-xs-6 col-sm-4 col-sm-offset-2">
+                <h3>Discreet Drop Blue</h3>
+                <p>For consistency, the blue color Discreet Drop uses is HEX #3FA9F5.</p>
+            </div>
+            <div class="col-xs-6 col-sm-4">
+                <div class="panel panel-blue">
+                </div>
+            </div>
+        </div>
+        <div class="row always-equal">
+            <div class="col-xs-6 col-sm-4 col-sm-offset-2">
+                <h3>Discreet Drop Black</h3>
+                <p>For consistency, the black color Discreet Drop uses is HEX #1A1A1A.</p>
+            </div>
+            <div class="col-xs-6 col-sm-4">
+                <div class="panel panel-black">
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="section">
+        <h2 class="center">Discreet Drop 60 Second Explainer Video</h2>
+        <div class="row">
+            <div class="col-sm-6 col-sm-offset-3 center">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/gDi286SFsBE"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="section">
+        <div class="row">
+            <div class="col-sm-10 col-sm-offset-1">
+                <h2>Press Info</h2>
+                <p>All the contents on this page are provided free to use as you wish, with high-quality assets and information available to get you started. clean, high resolution logos and icons with transparent backgrounds have been provided on this page in both .SVG and .PNG formats depending on your needs. A linked explainer video has also been provided that may be linked-to and shared to help quickly explain what Discreet Drop does and the benefits and value we offer. For more information or general inquires about Discreet Drop, feel free to contact us by visiting <a href="/contact">our contact page here</a> and sending us an email at any time, and we'll be happy to help answer any questions, or just chat. We're friendly!</p>
+            </div>
+        </div>
+    </div>
+    
+    <div class="section">
+        @include('partials.about_content')
+    </div>
+    
     
 </div>
 
